b2376615496e02563753b5922abe46bb4c4f83ed
danlipatov <danila.lipatov@ratings.ru>
2025-07-09 15:21:22 +0300

    added upload file + date

--- internal_chat/views.py
+++ internal_chat/views.py
@@ -1,15 +1,23 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .models import Message
-
+from .forms import MessageForm
 
 @login_required
 def chat_room(request):
-    if request.method == "POST":
-        message = request.POST.get("message")
-        if message:
-            Message.objects.create(sender=request.user, content=message)
-        return redirect("chat_room")
+    messages = Message.objects.select_related('sender').order_by('-timestamp')[:50][::-1]
 
-    messages = Message.objects.select_related("sender").all().order_by("-timestamp")[:50]
-    return render(request, "chat_room.html", {"messages": messages})
+    if request.method == 'POST':
+        form = MessageForm(request.POST, request.FILES)
+        if form.is_valid():
+            msg = form.save(commit=False)
+            msg.sender = request.user
+            msg.save()
+            return redirect('chat_room')
+    else:
+        form = MessageForm()
+
+    return render(request, 'chat_room.html', {
+        'messages': messages,
+        'form': form
+    })
